7b0b8591262a43b49ded64bff791b41f0e50b799
Georgie Kirschner <georgie@montanacodeschool.com>
2019-04-26 13:45:47 -0600

    adding sum exercises to calculations

--- src/calculations.js
+++ src/calculations.js
@@ -6,16 +6,18 @@ console.log(util);
 // step 1. run the file to look at the util object
 
 // step 2. UNCOMMENT the line below, then execute the compiled file
-//console.log(util.multiplyBy2(3));
+// console.log(util.multiplyBy2(3));
+
+// => output should be "6"
 // look at how we have used a modules function
 
-// step 3. Create a new function called splitSpace in the stringManipulation module (stringManipulation.js)
-// that splits a string apart by each character, and returns that array of each
-// characters. Uncomment the code below to make it run.
+// step 3. Create a new function called "sum" in the mathUtil module (mathUtil.js)
+// that calculates the sum of two numbers.
+// Uncomment the code below to make it run.
 
 //UNCOMMENT lINE BELOW IN STEP 3
-//console.log(util.splitSpace('feeling'))
+//console.log(util.sum(27, 62));
 
-// => output should be ['f', 'e', 'e', 'l', 'i', 'n', 'g']
+// => output should be "89"
 
-//Nice work! Your utilizing the module pattern
+// Nice work! Your utilizing the module pattern
