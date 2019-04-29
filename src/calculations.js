54ff57ba37842c22a4a52aae82ad6cc2dc98cfd9
Georgie Kirschner <georgie@montanacodeschool.com>
2019-04-29 11:07:52 -0600

    multiple exports in mathUtil

--- src/calculations.js
+++ src/calculations.js
@@ -1,29 +1,31 @@
-// import everything ("*") from the file "mathUtil.js" in
+// Import everything ("*") from the file "mathUtil.js" in
 // the same directory calling it "util" in this file ("as util")
 import * as util from "./mathUtil";
 console.log(util);
 
-// step 1. run the file to look at the util object
+// Step 1. run the file to look at the util object
 
-// step 2. UNCOMMENT the line below, then execute the compiled file
+// Step 2. UNCOMMENT the line below, compile and then execute the ./lib/calculations.js file
 //console.log(util.multiplyBy11(12));
 
 // => output should be "132"
 // look at how we have used a modules function
 
-// step 3. Create a new function called "sum" in the mathUtil module (mathUtil.js)
+// Step 3. Create a new function called "sum" in the mathUtil module (mathUtil.js)
 // that calculates the sum of two numbers.
-// Uncomment the code below to make it run.
-
-//UNCOMMENT lINE BELOW IN STEP 3
+// Uncomment the code below, compile and then execute the ./lib/calculations.js file
 //console.log(util.sum(27, 62));
 
-// => output should be "89"
+// => Output should be "89"
 
-// Nice work! Your utilizing the module pattern
+// Nice work! You are utilizing the module pattern
 
 // step 4. Remove the line which starts with export in the mathUtil module.
-//TBD EXPORT FOR EVERY VAR
+// Put the keyword "export" in front of both "const" keywords in the mathUtil module.
+// Compile and re-execute the ./lib/calculations.js file
+
+// It still works! Two different ways to accomplish the same thing = JavaScript ;)
+
 // TBD IMPORT SINGLE FUNCTION { sum } with naming {sum as sumAll}
 // TBD NPM INSTALL RANDOM LIBRARY AND USE IT
 // TBD EXPORT DEFAULT
